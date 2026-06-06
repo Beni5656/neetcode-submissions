41b6734da9a5b632d2e8713ae68610d5a78e4cf7
Benedict Michel <beni_michel@berkeley.edu>
2026-06-06 16:11:35 -0500

    Add: maximum-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-subarray/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSoFar = nums[0];
+        int currSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (currSum < 0) {
+                currSum = 0;
+            }
+
+            currSum += nums[i];
+
+            maxSoFar = Math.max(maxSoFar, currSum);
+        }
+
+        return maxSoFar;
+    }
+}
