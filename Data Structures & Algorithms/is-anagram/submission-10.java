cecf0d5993434b88676d63e26a697f751344057f
Benedict Michel <beni_michel@berkeley.edu>
2026-06-18 18:18:45 -0500

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] s1 = new int[26];
+        int[] t1 = new int[26];
+
+        for (char c: s.toCharArray()) {
+            s1[c - 'a']++;
+        }
+
+        for (char c: t.toCharArray()) {
+            t1[c - 'a']++;
+        }
+
+        return Arrays.equals(s1, t1);
+    }
+}
