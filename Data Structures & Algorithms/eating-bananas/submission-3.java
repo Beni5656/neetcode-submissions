eff17ac427f9ecd7113c0dbecfe6c310416a4a34
Benedict Michel <beni_michel@berkeley.edu>
2026-07-16 16:56:26 -0500

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = 1;
+
+        for (int pile: piles) {
+            right = Math.max(right, pile);
+        }
+
+        int result = right;
+
+        while (left <= right) {
+            int midSpeed = (left + right) / 2;
+
+            if (canFinish(piles, h, midSpeed)) {
+                right = midSpeed - 1;
+                result = midSpeed;
+            } else {
+                left = midSpeed + 1;
+            }
+        }
+
+        return result;
+    }
+
+    private boolean canFinish(int[] piles, int h, int speed) {
+        int totalHours = 0;
+
+        for (int pile: piles) {
+            totalHours += (pile + speed - 1) / speed;
+        }
+
+        return totalHours <= h;
+    }
+}
