c714b11b6a9e31f1733bc121c66ecc71db0147ba
Benedict Michel <beni_michel@berkeley.edu>
2026-08-06 15:48:01 -0500

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] array = new int[n];
+        
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        array[0] = 1;
+        array[1] = 2;
+
+        for (int i = 2; i < n; i++) {
+            array[i] = array[i - 2] + array[i - 1];
+        }
+
+        return array[n - 1];
+    }
+}
