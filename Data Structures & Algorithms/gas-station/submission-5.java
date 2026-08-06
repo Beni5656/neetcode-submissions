e5056b9530d2213dad0b2e65715ac4f373b5f202
Benedict Michel <beni_michel@berkeley.edu>
2026-08-06 17:04:42 -0500

    Add: gas-station - submission-5

---
+++ Data Structures & Algorithms/gas-station/submission-5.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            total += gas[i] - cost[i];
+        }
+
+        if (total < 0) return -1;
+
+        total = 0;
+        int station = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            total += gas[i] - cost[i];
+
+            if (total < 0) {
+                station = i + 1;
+                total = 0;
+            }
+        }
+
+        return station;
+    }
+}
