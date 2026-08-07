136bc85f0ac708ba618b562a601cd1bb4eb70d9d
Benedict Michel <beni_michel@berkeley.edu>
2026-08-07 18:17:48 -0500

    Add: last-stone-weight - submission-5

---
+++ Data Structures & Algorithms/last-stone-weight/submission-5.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        Queue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int stone: stones) {
+            queue.offer(stone);
+        }
+
+        while (queue.size() > 1) {
+            int stone1 = queue.poll();
+            int stone2 = queue.poll();
+
+            if (stone1 == stone2) continue;
+
+            queue.offer(stone1 - stone2);
+        }
+
+        return queue.isEmpty() ? 0 : queue.peek();
+    }
+}
